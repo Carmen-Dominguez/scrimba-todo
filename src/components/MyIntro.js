21110b3bd6e463c97d919718f39133e4b4cd5c0a
Carmen D <carmengdominguez94@gmail.com>
2021-05-14 16:10:21 +0200

    intro modification

--- src/components/MyIntro.js
+++ src/components/MyIntro.js
@@ -2,9 +2,22 @@ import React from "react";
 
 function MyIntro() {
     const name = "Cassie";
+
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    
+    if (hours < 12) {
+        timeOfDay = "morning"
+    } else if (hours >= 12 && hours <= 17) {
+        timeOfDay = "afternoon"
+    } else {
+        timeOfDay = "evening"
+    }
+
     return (
         <main>
-            <h3>Hi, I'm {name}</h3>
+            <h3>Good {timeOfDay}, I'm {name}</h3>
             <p>I'm not a fan of my name much, so I'm going with {name} for now. One day I'd like to travel to one
             of these places:
         </p>
