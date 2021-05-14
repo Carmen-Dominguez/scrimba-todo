1aa6cfbee57624d52cc1b570e7a328d81293718a
Carmen D <carmengdominguez94@gmail.com>
2021-05-14 16:51:18 +0200

    time styling on intro

--- src/components/MyIntro.js
+++ src/components/MyIntro.js
@@ -3,21 +3,27 @@ import React from "react";
 function MyIntro() {
     const name = "Cassie";
 
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+    const styles= {
+        color: "inherit"
+    };
     
     if (hours < 12) {
-        timeOfDay = "morning"
+        timeOfDay = "morning";
+        styles.color = "#d7ebed";
     } else if (hours >= 12 && hours <= 17) {
-        timeOfDay = "afternoon"
+        timeOfDay = "afternoon";
+        styles.color = "#b3dbe0";
     } else {
-        timeOfDay = "evening"
+        timeOfDay = "evening";
+        styles.color = "#141852";
     }
 
     return (
         <main>
-            <h3>Good {timeOfDay}, I'm {name}</h3>
+            <h3 style={styles}>Good {timeOfDay}, I'm {name}</h3>
             <p>I'm not a fan of my name much, so I'm going with {name} for now. One day I'd like to travel to one
             of these places:
         </p>
