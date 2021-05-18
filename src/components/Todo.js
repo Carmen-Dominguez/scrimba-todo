1c0a6027f7eb0b64bf575fd65e3de24006193a34
Carmen D <carmengdominguez94@gmail.com>
2021-05-18 12:48:51 +0200

    render array todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,21 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
+const myTodos = [
+    { id: "1", task: "Todo 1"},
+    { id: "2", task: "Todo 2"},
+    { id: "3", task: "Todo 3"},
+    { id: "4", task: "Todo 4"},
+];
+
 function Todo() {
+    const todos = myTodos.map(item => <TodoItem id={item.id} task={item.task} />);
+
     return (
         <div>
             <h3>My Todo List: </h3>
             <form>
-                <TodoItem id={"1"} task={"Todo 1 "} />
-                <TodoItem id={"2"} task={"Todo 2 "} />
-                <TodoItem id={"3"} task={"Todo 3 "} />
+                {todos}
             </form>
         </div>
     );
