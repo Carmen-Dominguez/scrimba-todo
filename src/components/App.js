e8ad4db8595ec7ee25a8c2283463188d109ccdf6
Carmen D <carmengdominguez94@gmail.com>
2021-05-18 16:40:07 +0200

    implement app state

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,20 @@ import Todo from "./Todo";
 import Footer from "./Footer";
 
 class App extends React.Component {
-    name = "Cassie";
+    constructor() {
+        super();
+        this.state = {
+            name: "Cassie D"
+        }
+    }
 
     render() {
         return (
             <div className="container">
                 <Header />
-                <MyIntro name={this.name}/>
+                <MyIntro name={this.state.name}/>
                 <Todo />
-                <Footer name={this.name}/>
+                <Footer name={this.state.name}/>
             </div>
         );
     }
